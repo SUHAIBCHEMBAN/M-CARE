e49fae5f5932fdc9fff962673dd792ea7cebbed9
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-24 12:02:41 +0530

    Update settings.py

--- mchproject/mchproject/settings.py
+++ mchproject/mchproject/settings.py
@@ -132,18 +132,33 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# import os
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# new
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+# This should be a different directory than STATIC_ROOT
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# Directory where collectstatic will collect static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # email backend
 
