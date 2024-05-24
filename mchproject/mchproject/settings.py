f2066e10b34bf1573368ce4fb0c0dbd63e1939ac
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-24 10:48:06 +0530

    Update settings.py

--- mchproject/mchproject/settings.py
+++ mchproject/mchproject/settings.py
@@ -137,10 +137,13 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # email backend
 
