f9960b308f000db1f177aaf34397c21004ed9083
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-27 09:22:27 +0530

    Update settings.py

--- mchproject/mchproject/settings.py
+++ mchproject/mchproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*$2nk8(m25!!lrtoxmo9^5y*(yoe047yb2ahb%9xz(ifrp@14+'
 
-ALLOWED_HOSTS = ['52.23.150.25', 'localhost']
+ALLOWED_HOSTS = ['52.23.150.25', 'localhost', 'mcareclinicservice.live', 'www.mcareclinicservice.live']
 
 # INTERNAL_IPS = ['127.0.0.1', '::1'] 
 
