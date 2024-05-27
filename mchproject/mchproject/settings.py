5521e8d12e3d49c4f70363a996bf01af4086f64a
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-27 09:32:38 +0530

    Update settings.py

--- mchproject/mchproject/settings.py
+++ mchproject/mchproject/settings.py
@@ -131,9 +131,6 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # This directory is for development static files
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
