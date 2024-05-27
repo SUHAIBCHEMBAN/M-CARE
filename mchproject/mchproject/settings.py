ef5fa2d697c0c892c6ae6ddc0531f51793bef0c0
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-27 14:14:41 +0530

    Update settings.py

--- mchproject/mchproject/settings.py
+++ mchproject/mchproject/settings.py
@@ -148,8 +148,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True    
-EMAIL_HOST_USER = 'shuhaibc2004@gmail.com' 
-EMAIL_HOST_PASSWORD = 'zwra otta kpzg kubz'
+EMAIL_HOST_USER = 'mcareclinic2.0@gmail.com' 
+# shuhaibc2004@gmail.com
+EMAIL_HOST_PASSWORD =  'vbwa vakq xnsg obbt'
+# 'zwra otta kpzg kubz'
 
 # Media url
 
