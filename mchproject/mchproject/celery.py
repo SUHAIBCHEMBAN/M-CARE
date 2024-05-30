b46ea1ad2dc1c8e23a536ada32a3378f02821db2
SUHAIB CHEMBAN <151101554+SUHAIBCHEMBAN@users.noreply.github.com>
2024-05-30 09:50:56 +0530

    Update celery.py

--- mchproject/mchproject/celery.py
+++ mchproject/mchproject/celery.py
@@ -1,23 +1,23 @@
-# from __future__ import absolute_import, unicode_literals
-# import os
-# from celery import Celery
-# from django.conf import settings
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mchproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mchproject.settings')
 
-# app = Celery('mchproject')
-
-
-# app.conf.enable_utc = False
-# app.conf.update(timzone = 'Asia/Kolkata')
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('mchproject')
 
 
-# app.autodiscover_tasks()
-
-# @app.task(bind=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
+app.conf.enable_utc = False
+app.conf.update(timzone = 'Asia/Kolkata')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 
-# CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
+
+
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
